c0a6211c91e7e9fea89925a1ce373a7d17435c42
MossTek <moss.personal.email@gmail.com>
2023-04-22 00:17:56 -0400

    just adding comments

--- AsteriskFreePBXInstall.sh
+++ AsteriskFreePBXInstall.sh
@@ -1,14 +1,29 @@
-apt update &&  apt -y full-upgrade
-apt install -y git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev uuid-dev
-add-apt-repository -y universe
+#!/usr/bin/bash
+#this script is to install and configure Asterisk18 + FreePBX
+
+#check to see if the user is the root user
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root. Exiting..."
+  exit 1
+fi
+
+#install dependencies and upgrade
+add-apt-repository -y ppa:ondrej/php ppa:universe
 apt update -y
-apt install -y subversion
+apt install -y 'git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev uuid-dev software-properties-common php7.4 subversion apache2 mariadb-server php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gdphp7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath nodejs npm subversion' &&
+apt upgrade -y
+
+#download and estract asterisk archive
 cd /usr/src/
 curl -O http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
 tar xvf asterisk-18-current.tar.gz
+
+#Default config for asterisk
 cd asterisk-18*/
 contrib/scripts/install_prereq -y install
 ./configure
+
+#specifying make options
 make menuselect.makeopts
 ENABLE_CATEGORIES="MENUSELECT_ADDONS"
 for cat in $ENABLE_CATEGORIES ; do
@@ -52,6 +67,8 @@ make install
 make samples
 make config
 ldconfig
+
+#add asterisk user
 cd /root/
 groupadd asterisk
 useradd -r -d /var/lib/asterisk -g asterisk asterisk
@@ -59,28 +76,28 @@ usermod -aG audio,dialout asterisk
 chown -R asterisk.asterisk /etc/asterisk
 chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
 chown -R asterisk.asterisk /usr/lib/asterisk
+
+#configure /etc/default/asterisk and asterisk.conf
 sed '/#AST_USER="asterisk"/s/^#//' -i /etc/default/asterisk
 sed '/#AST_GROUP="asterisk"/s/^#//' -i /etc/default/asterisk
 sed '/runuser = asterisk ; The user to run as./s/^#//' -i /etc/asterisk/asterisk.conf
 sed '/rungroup = asterisk ; The group to run as./s/^#//' -i /etc/asterisk/asterisk.conf
+
+#restart asterisk daemon
 systemctl restart asterisk
 systemctl enable asterisk
-apt install mariadb-server
-apt install apache2
+
+#configure apache config and store default
 cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf_orig
 sed -i 's/^\(User\|Group\).*/\1 asterisk/' /etc/apache2/apache2.conf
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 rm -f /var/www/html/index.html
 unlink /etc/apache2/sites-enabled/000-default.conf
-apt-get -y update
-apt -y install software-properties-common
-add-apt-repository -y ppa:ondrej/php
-apt-get -y update
-apt -y install php7.4
-apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gdphp7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
 sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php/7.4/apache2/php.ini
 sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php/7.4/cli/php.ini
 sed -i 's/\(^memory_limit = \).*/\1256M/' /etc/php/7.4/apache2/php.ini
+
+#download FreePBX 16 archive
 cd /root/
 wget http://mirror.freepbx.org/modules/packages/freepbx/7.4/freepbx-16.0-latest.tgz
 tar xfz freepbx-16.0-latest.tgz
@@ -88,26 +105,30 @@ rm -f freepbx-16.0-latest.tgz
 cd freepbx
 systemctl stop asterisk
 ./start_asterisk start
-apt install nodejs npm
 ./install -n
 fwconsole ma disablerepo commercial
 fwconsole ma installall
 fwconsole ma delete firewall
 fwconsole reload
 fwconsole restart
+
+#finish apache config
 a2enmod rewrite
 systemctl restart apache2
+
+#enable isp ssh http https and ssh
 ufw enable
 ufw allow 5060
 ufw allow 5061
 ufw allow ssh
 ufw allow http
 ufw allow https
-apt update
-apt install fail2ban
+apt -y update
+
+#install and configure fail2ban
+apt -y install fail2ban
 systemctl status fail2ban.service
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sed -i 's/\/var\/log\/asterisk\/messages/\/var\/log\/asterisk\/full/g' /etc/fail2ban/jail.local
 systemctl enable fail2ban
 systemctl start fail2ban
-ufw status
